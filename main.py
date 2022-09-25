0e2fea8ac012f228a119a49c7b098efedfba347f
elianderson-dev <nin2333124568@gmail.com>
2022-09-25 09:01:59 +0000

    feat: adds the get endpoint

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from json import dumps
 
 db_connect = create_engine("sqlite:///test.db")
 app = Flask(__name__)
-api - Api(app)
+api = Api(app)
 
-
+class Users(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select * from user")
+        result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+        return jsonify(result)
