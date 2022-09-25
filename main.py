fc543346ffbbba0b18128aa4ce32cf2223b2c3e2
elianderson-dev <nin2333124568@gmail.com>
2022-09-25 10:30:22 +0000

    feat: adds delete endpoint for send id

--- main.py
+++ main.py
@@ -42,3 +42,11 @@ class Users(Resource):
         query = conn.execute("select * from user where id=%d " % int(id))
         result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
         return jsonify(result)
+
+# Send ID with
+
+class UserById(Resource):
+    def delete(self, id):
+        conn = db_connect.connect()
+        conn.execute("delete from user where id=%d " % int(id))
+        return {"status": "sucesss"}
